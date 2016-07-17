c40e94e7b48e5960a3a3d1bbf4402366d213c272
robfauli <robfauli.oss@gmail.com>
2016-07-17 22:07:56 +0200

    Implemented setters and getters for position and color of the LightSource class.

--- Visualization/Shapes/LightSource.hpp
+++ Visualization/Shapes/LightSource.hpp
@@ -6,14 +6,27 @@
 class LightSource
 {
 public:
-    LightSource(glm::vec3 position) : _position(position) { };
+    LightSource(const glm::vec3 &position) : _position(position) { };
 
     const auto &getPosition() {
         return _position;
     }
 
+    const auto &getLightColor() {
+        return _lightColor;
+    }
+
+    void setPosition(const glm::vec3 &position) {
+        _position = position;
+    }
+
+    void setLightColor(const glm::vec3 &color) {
+        _lightColor = color;
+    }
+
 private:
     glm::vec3 _position;
+    glm::vec3 _lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
 };
 
 
